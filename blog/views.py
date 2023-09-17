63a46633740076ed8d07ac73206a412a66f16d12
rustam <rustam.raimanovv@gmail.com>
2023-09-17 13:22:42 +0500

    dz (3.8) Наследование шаблонов

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,63 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from django.template.loader import render_to_string
+from random import sample
 
-
-# list_guinness_records = ['Мощный удар', 'Гамбургер', 'Человек-ёж']
+data_posts = [
+    {
+        'title': 'Рыбалка',
+        'description': 'Хорошо посидели',
+        'date': '21 авг 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+    },
+    {
+        'title': 'Париж',
+        'description': 'Незабываемое путешествие',
+        'date': '5 сент 2020',
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+    },
+    {
+        'title': 'Финал лиги чемпионов',
+        'description': 'Реал опять выиграл ЛЧ',
+        'date': '28 мая 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+    },
+    {
+        'title': 'Охота на уток',
+        'description': 'Ни одна утка не пострадала',
+        'date': '7 дек 2019',
+        'content': 'Lorem ipsum dolor sit amet.'
+    },
+    {
+        'title': 'Фестиваль огурца',
+        'description': 'Суздаль ждет тебя',
+        'date': '12 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+    },
+    {
+        'title': 'Нашествие',
+        'description': 'Даешь рок, но в следующем году',
+        'date': '29 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+    },
+    {
+        'title': 'Новый год',
+        'description': 'Эх, еще один год пролетел',
+        'date': '31 дек 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+    },
+]
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    rand_posts = sample(data_posts, 3)
+    data = {
+        'rand_posts': rand_posts
+    }
+    return render(request, 'blog/index.html', context=data)
 
 
 def posts(request):
@@ -57,10 +105,3 @@ def guinness_records(request):
         'count_needle': 1790,
     }
     return render(request, 'blog/guinness_records.html', context=data)
-
-# def guinness_records(request):
-#     li_elements = ''
-#     for record in list_guinness_records:
-#         li_elements += f"<li>{record.title()}</a></li>"
-#     response = f'<ol>{li_elements}</ol>'
-#     return HttpResponse(response)
